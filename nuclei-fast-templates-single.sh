1493e913473749fbaad8a62e6d0d4bd8668a2bf5
DIMOOON <68978608+reewardius@users.noreply.github.com>
2025-05-02 22:59:40 +0300

    Update nuclei-fast-templates-single.sh

--- nuclei-fast-templates-single.sh
+++ nuclei-fast-templates-single.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Выход при любой ошибке
+# Р’С‹С…РѕРґ РїСЂРё Р»СЋР±РѕР№ РѕС€РёР±РєРµ
 set -e
 
 nuclei -l alive_http_services.txt -eid web-cache-poisoning,cache-poisoning-fuzz -etags fuzzing-req,logs,backup,listing,android,ios,ssrf -rl 1000 -c 100 -t nuclei-fast-templates/ -o nuclei_fast_templates.txt
